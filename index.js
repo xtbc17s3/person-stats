e49d331c3c6280585b4263b1e4f055ed69c69148
Dave Strus <dave@getfretless.com>
2017-06-26 16:25:29 -0400

    Handle clicks on the button and the instructions paragraph

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-function changeHeading() {
-  const heading = document.querySelector('h1')
-  heading.textContent = 'Ye Olde Person Stats'
+function changeHeading(ev) {
+  ev.target.textContent = 'Clickity click'
 }
 
 const btn = document.querySelector('button')
 btn.addEventListener('click', changeHeading)
+
+const p = document.querySelector('#instructions')
+p.addEventListener('click', changeHeading)
