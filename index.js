5eca4406498d299f3f6b74f2609c8201bf4ed082
Dave Strus <dave@getfretless.com>
2017-06-27 14:21:17 -0400

    Add age to the form and the stats

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ function handleSubmit(ev) {
   ev.preventDefault()
   const f = ev.target
   const name = f.personName.value
+  const age = f.personAge.value
   const div = document.querySelector('#stats')
-  div.innerHTML = '<p>' + name + '</p>'
+  div.innerHTML = '<p>' + name + ', age ' + age + '</p>'
 }
 
 const personForm = document.querySelector('#person-form')
