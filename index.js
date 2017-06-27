4705da6c041cc9c980302ae1151cdc20a881ba8c
Dave Strus <dave@getfretless.com>
2017-06-27 10:37:44 -0400

    Display stats in the person's favorite color

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 function handleSubmit(ev) {
   ev.preventDefault()
   const f = ev.target
+
   const name = f.personName.value
   const age = f.age.value
-  const stats = document.querySelector('#stats')
-  // stats.innerHTML = '<p>' + name + ', age ' + age + '</p>'
-  // stats.innerHTML = `<p>${name}, age ${age}</p>`
+  const favoriteColor = f.favoriteColor.value
 
   const p = document.createElement('p')
-  p.textContent = `${name}, age ${age}`
+  p.textContent = `${name}, age ${age} ${favoriteColor}`
+  p.style.backgroundColor = favoriteColor
+  // p.setAttribute('id', name)
 
+  const stats = document.querySelector('#stats')
   stats.appendChild(p)
+
+  // stats.innerHTML = '<p>' + name + ', age ' + age + '</p>'
+  // stats.innerHTML = `
+  //   <p style="background-color: ${favoriteColor}">
+  //     ${name}, age ${age}
+  //   </p>
+  // `
 }
 
 const personForm = document.querySelector('#person-form')
