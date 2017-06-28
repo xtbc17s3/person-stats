455992472f866f24dec67a7115caaeea71dec48c
Dave Strus <dave@getfretless.com>
2017-06-28 08:12:17 -0400

    Use a description list (dl)

--- index.js
+++ index.js
@@ -8,17 +8,24 @@ function renderColor(color) {
 
 function renderListItem(label, value) {
   const item = document.createElement('li')
-  item.textContent = `${label}: `
+  const dt = document.createElement('dt')
+  dt.textContent = label
+
+  const dd = document.createElement('dd')
+
   try {
-    item.appendChild(value)
+    dd.appendChild(value)
   } catch (e) {
-    item.textContent += value
+    dd.textContent += value
   }
+
+  item.appendChild(dt)
+  item.appendChild(dd)
   return item
 }
 
 function renderList(data) {
-  const list = document.createElement('ul')
+  const list = document.createElement('dl')
 
   Object.keys(data).map(label => {
     const item = renderListItem(label, data[label])
